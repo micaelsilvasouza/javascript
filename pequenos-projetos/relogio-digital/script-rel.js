a9fc3bf8bcd9dc80a9c4e1e71822c1ea683b44ef
micaelsilvasouza <108833081+micaelsilvasouza@users.noreply.github.com>
2022-10-04 12:34:40 -0300

    Script terminado e  funcionando

---
+++ pequenos-projetos/relogio-digital/script-rel.js
@@ -0,0 +1,11 @@
+function Hora(){
+    let data = new Date()
+    let h = data.getHours()
+    let m = data.getMinutes()
+    let s = data.getSeconds()
+
+    rel.innerHTML = `${h}:${m}:${s}`
+}
+
+setInterval(Hora, 1000)
+Hora()
