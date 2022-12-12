549e4d45938bbf9bac1132c2d50a1d29cfd819dd
micaelsilvasouza <108833081+micaelsilvasouza@users.noreply.github.com>
2022-12-12 19:28:41 -0300

    Ainda falta corrigir algum erro

---
+++ testando-classes/modularizacao/aprendendo/Person.js
@@ -0,0 +1,7 @@
+export class Person{
+    constructor(nome){
+        this.nome = nome
+        this.forca = 100
+        this.defesa = 100
+    }
+}
---
+++ testando-classes/modularizacao/aprendendo/main.js
@@ -0,0 +1,7 @@
+import { Person } from "./Person.js"
+
+main.innerHTML += "<p>Um Modulo. Reutilizando uma classe</p>"
+
+let p1 = new Person("Ariovaldo")
+
+main.innerHTML += `${p1.nome}, possui força de ${p1.forca} e uma desfesa de ${p1.defesa}.`
