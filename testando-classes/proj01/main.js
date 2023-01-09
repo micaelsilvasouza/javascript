e1be71ecd8c42363aa586ec3d6d5c97f9187c675
micaelsilvasouza <108833081+micaelsilvasouza@users.noreply.github.com>
2023-01-09 19:39:30 -0300

    Classe Item criada

--- testando-classes/proj01/main.js
+++ testando-classes/proj01/main.js
@@ -16,7 +16,7 @@ class Person{
                     Ataque = ${this.#ataque}<br>
                     Defesa = ${this.#defesa}<br>
                     Magia = ${this.#magia}<br>
-                    Atributo Magico = ${this.#atrMagico}
+                    Atributo Magico = ${this.#atrMagico}<br>
                 </p>`
     }
 
@@ -61,4 +61,54 @@ class Person{
     }
 }
 
-let p1 = new Person("Juvenildo", "fogo")
+class Item{
+    #tipo
+    #nome
+    #bonus
+
+    constructor(nome, tipo, bonus){
+        this.#nome = nome
+        this.#tipo = tipo
+        this.bonus = bonus
+    }
+
+    propriedades(){
+        return `<p>
+                    Nome = ${this.#nome}<br>
+                    Tipo = ${this.#tipo}<br>
+                    bonus:<br>
+                    Ataque = ${this.#bonus[0]}<br>
+                    Defesa = ${this.#bonus[1]}<br>
+                    Magia = ${this.#bonus[2]}<br>
+                </p>`
+    }
+
+    get tipo(){
+        return this.#tipo
+    }
+
+    set tipo(tipo){
+        this.#tipo = tipo
+    }
+
+    get nome(){
+        return this.#nome
+    }
+
+    set nome(nome){
+        this.#nome = nome
+    }
+
+    get bonus(){
+        return this.#bonus
+    }
+
+    set bonus(bonus){
+        if(typeof(bonus) === typeof([])){
+            this.#bonus = bonus
+        }else{
+           console.log("[ERRO] O atributo bonus exige que seu valor seja uma array(vetor)")
+        }
+    }
+}
+
