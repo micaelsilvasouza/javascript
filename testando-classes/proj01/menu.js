47e90e8f90e619809d0dc63334b9d9a94cc6bbd1
micaelsilvasouza <108833081+micaelsilvasouza@users.noreply.github.com>
2023-01-13 19:30:00 -0300

    Interface criada

---
+++ testando-classes/proj01/menu.js
@@ -0,0 +1,17 @@
+btcriar.onclick = function(){
+    campCriar.style.display = 'block'
+    campItens.style.display = "none"
+    campStatus.style.display = "none"
+}
+
+btitem.onclick = function(){
+    campCriar.style.display = 'none'
+    campItens.style.display = "block"
+    campStatus.style.display = "none"
+}
+
+btstatus.onclick = function(){
+    campCriar.style.display = 'none'
+    campItens.style.display = "none"
+    campStatus.style.display = "block"
+}
