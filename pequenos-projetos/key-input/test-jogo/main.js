1a39250eb06106c86294309b87cad94a936e5218
micaelsilvasouza <108833081+micaelsilvasouza@users.noreply.github.com>
2023-05-27 12:22:30 -0300

    Timer e um coletavel

--- pequenos-projetos/key-input/test-jogo/main.js
+++ pequenos-projetos/key-input/test-jogo/main.js
@@ -2,6 +2,10 @@
 //Recebendo o input e a div cubo do  html
 let input = document.getElementById("input")
 let cubo = document.getElementById("cubo")
+let timer = document.getElementById("timer")
+//Dimenções da tela
+let larg = window.innerWidth
+let altu = window.innerHeight
 //Definindo as variaveis de posicionamento
 let posx = 200
 let posy = 200
@@ -24,7 +28,7 @@ function inicairMove(event){
     */
     input.value = ""
     key = event.key.toLowerCase()//tecla precionada coloca em minusculo
-    console.log(key)
+    //console.log(key)
     //Verificando a tecla precionada
     if(!movimentando && key == "a" || key == "arrowleft"){
         //execultando movimento para esquerda
@@ -58,24 +62,52 @@ function pararMove(){
 
 function moverEsq(){
     //Função que move a div cubo para a esquerda em 5px
-    posx -= 10
-    cubo.style.left = posx+"px"
+    if(posx > 30){
+        posx -= 10
+        cubo.style.left = posx+"px"
+    }
+    
 }
 
 function moverDir(){
     //Função que move a div cubo para a direita em 5 px
-    posx+=10
-    cubo.style.left = posx+"px"
+    if(posx < larg - 40){
+        posx+=10
+        cubo.style.left = posx+"px"
+    }
+    
 }
 
 function moverCima(){
     //Função que move a div cubo para cima em 5px
-    posy -=10
-    cubo.style.top = posy+"px"
+    if(posy > 30){
+        posy -=10
+        cubo.style.top = posy+"px"
+    }
+    
 }
 
 function moverBaixo(){
     //Função que move a div cubo para baixa em 5px
-    posy +=10
-    cubo.style.top = posy+"px"
+    if(posy < altu-40){
+        posy +=10
+        cubo.style.top = posy+"px" 
+    }
+}
+
+
+function iniciarTimer(time){
+    timer.innerText = time
+    let intervalo = setInterval(()=>{
+        if(time > 0){
+            time--
+            timer.innerText = time
+        }else{
+            pararTimer(intervalo)
+        }
+    }, 1000)
+}
+
+function pararTimer(intervalo){
+    clearInterval(intervalo)
 }
