3f4267c7e5a6c3657d055f770b164c3e05a9a5e6
micaelsilvasouza <108833081+micaelsilvasouza@users.noreply.github.com>
2023-05-29 12:35:40 -0300

    Detecção de colisão

---
+++ pequenos-projetos/key-input/test-jogo/js/timer.js
@@ -0,0 +1,40 @@
+let timer_intervalo
+let time = 10
+function iniciarTimer(){
+    let min = Math.floor(time/60)
+    let seg = time%60
+    minstr = min >= 10? String(min) : "0"+min
+    segstr = seg >= 10? String(seg) : "0"+seg
+    timer.innerText = `${minstr}:${segstr}`
+    timer_intervalo = setInterval(()=>{
+        if(seg > 0){
+            time--
+            seg--
+            minstr = min >= 10? String(minstr) : "0"+min
+            segstr = seg >= 10? String(seg):"0"+seg;
+            timer.innerText = `${minstr}:${segstr}`
+        }else if(min > 0){
+            min--
+            seg = 59
+            console.log(min)
+            minstr = min >= 10? String(min) : "0"+min
+            segstr = seg >= 10? String(seg):"0"+seg;
+            timer.innerText = `${minstr}:${segstr}`
+        }else{
+            pararTimer(timer_intervalo)
+            //window.location.href = "game-over.html"
+        }
+        return time
+    }, 1000)
+}
+
+function pararTimer(intervalo){
+    clearInterval(intervalo)
+}
+
+
+function atualizarTimer(newtime){
+    pararTimer(timer_intervalo)
+    time = newtime
+    iniciarTimer()
+}
--- pequenos-projetos/key-input/test-jogo/main.js
+++ pequenos-projetos/key-input/test-jogo/main.js
@@ -2,18 +2,24 @@
 //Recebendo o input e a div cubo do  html
 let input = document.getElementById("input")
 let cubo = document.getElementById("cubo")
+let boll = document.getElementById("boll")
 let timer = document.getElementById("timer")
+let pontos = document.getElementById("pontos")
+//iniciando o timer 
+time = 10
+iniciarTimer()
 //Dimenções da tela
 let larg = window.innerWidth
 let altu = window.innerHeight
 //Definindo as variaveis de posicionamento
-let posx = 200
-let posy = 200
+let cubo_posx = 200
+let cubo_posy = 200
+let boll_posx = 500
+let boll_posy = 300
 //Definindo a variavel de verificadação de movimento
 let movimentando = false
 //Definindo a variavel que armazenara o intervalo de movimento
-let move 
-
+let move_intervalo
 //Adicionado os eventos e funções do elemento input
 input.addEventListener("keydown", inicairMove)
 input.addEventListener("keyup", pararMove)
@@ -33,19 +39,19 @@ function inicairMove(event){
     if(!movimentando && key == "a" || key == "arrowleft"){
         //execultando movimento para esquerda
         movimentando = true
-        move = setInterval(moverEsq, 10)
+        move_intervalo = setInterval(moverEsq, 10)
     }else if(!movimentando && key == "d" || key == "arrowright"){
         //execultando movimento para a direita
         movimentando = true
-        move = setInterval(moverDir, 10)
+        move_intervalo = setInterval(moverDir, 10)
     }else if(!movimentando && key == "w" || key == "arrowup"){
         //execultando movimento para a cima
         movimentando = true
-        move = setInterval(moverCima, 10)
+        move_intervalo = setInterval(moverCima, 10)
     }else if(!movimentando && key == "s" || key == "arrowdown"){
         //execultando movimento para a baixo
         movimentando = true
-        move = setInterval(moverBaixo, 10)
+        move_intervalo = setInterval(moverBaixo, 10)
     }
 }
 
@@ -57,57 +63,69 @@ function pararMove(){
         Movimenot direito: 'd' ou seta esquerda
     */
     movimentando = false
-    clearInterval(move)
+    clearInterval(move_intervalo)
 }
 
 function moverEsq(){
     //Função que move a div cubo para a esquerda em 5px
-    if(posx > 30){
-        posx -= 10
-        cubo.style.left = posx+"px"
+    if(cubo_posx > 30){
+        cubo_posx -= 10
+        cubo.style.left = cubo_posx+"px"
+        verificarColisao()
     }
     
 }
 
 function moverDir(){
     //Função que move a div cubo para a direita em 5 px
-    if(posx < larg - 40){
-        posx+=10
-        cubo.style.left = posx+"px"
+    if(cubo_posx < larg - 40){
+        cubo_posx+=10
+        cubo.style.left = cubo_posx+"px"
+        verificarColisao()
     }
     
 }
 
 function moverCima(){
     //Função que move a div cubo para cima em 5px
-    if(posy > 30){
-        posy -=10
-        cubo.style.top = posy+"px"
+    if(cubo_posy > 30){
+        cubo_posy -=10
+        cubo.style.top = cubo_posy+"px"
+        verificarColisao()
     }
     
 }
 
 function moverBaixo(){
     //Função que move a div cubo para baixa em 5px
-    if(posy < altu-40){
-        posy +=10
-        cubo.style.top = posy+"px" 
+    if(cubo_posy < altu-40){
+        cubo_posy +=10
+        cubo.style.top = cubo_posy+"px" 
+        verificarColisao()
     }
 }
 
-
-function iniciarTimer(time){
-    timer.innerText = time
-    let intervalo = setInterval(()=>{
-        if(time > 0){
-            time--
-            timer.innerText = time
-        }else{
-            pararTimer(intervalo)
-        }
-    }, 1000)
+function gerarPontosColisao(pos, tam){
+    let points = []
+    for(let p = pos - tam/2; p <= pos + tam/2; p++){
+        points.push(p)
+    }
+    return points
 }
 
-function pararTimer(intervalo){
-    clearInterval(intervalo)
+function verificarColisao(){
+    let colidy = gerarPontosColisao(cubo_posy, 50) 
+    let colidx = gerarPontosColisao(cubo_posx, 50)
+    if(colidy.includes(boll_posy) && colidx.includes(boll_posx)){
+        boll_posx = Math.floor(Math.random() * (larg - 100) +1 ) +50
+        boll_posy = Math.floor(Math.random() * (altu - 100) +1 ) +50
+        mudarPosicaoBoll(boll_posx, boll_posy)
+        atualizarTimer(time + 1)
+        pontos.innerHTML = Number(pontos.innerHTML) + 1
+    }
+}
+
+function mudarPosicaoBoll(posx, posy){
+    boll.style.left = posx+"px"
+    boll.style.top = posy+"px"
 }
