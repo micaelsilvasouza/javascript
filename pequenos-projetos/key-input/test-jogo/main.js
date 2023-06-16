9c00ee8df943464ebacc6dd0a1ac40647e5b911d
micaelsilvasouza <108833081+micaelsilvasouza@users.noreply.github.com>
2023-06-16 13:17:55 -0300

    Corrigindo Problema de evento

--- pequenos-projetos/key-input/test-jogo/main.js
+++ pequenos-projetos/key-input/test-jogo/main.js
@@ -28,42 +28,68 @@ let movimentando = false
 //Definindo a variavel que armazenara o intervalo de movimento
 let move_intervalo
 //Adicionado os eventos e funções do elemento input
-input.addEventListener("keydown", inicairMove)
+input.addEventListener("keydown", inicairMoveTeclado)
 input.addEventListener("keyup", pararMove)
 //Adicionado os eventos e funçõe aos elementos buttons da classe botao
 for(let botao of botoes){
-    botao.addEventListener("touchstart", inicairMove)
+    botao.addEventListener("touchstart", inicairMoveBotao)
     botao.addEventListener("touchend", pararMove)
 }
 
-function inicairMove(event){
+function inicairMoveTeclado(event){
     /* 
         Função que verifica o event de precinamento de uma tecla do teclado, verificado as teclas especificas. 
         E cria o intervalos para o movimento da div cubo.
         Teclas especificadas:
         Movimento esquerdo: 'a' ou seta esquerda
         Movimento direito: 'd' ou seta esquerda
+        Movimento top: 'w' ou seta para cima
+        Movimento baixo: 's' ou seta para baixo
     */
     input.value = ""
     key = event.key.toLowerCase()//tecla precionada coloca em minusculo
+   
     //console.log(key)
     //Verificando a tecla precionada
-    if(!movimentando && (key == "a" || key == "arrowleft" || this.id == "esq")){
+    if(!movimentando && (key == "a" || key == "arrowleft")){
         //execultando movimento para esquerda
         movimentando = true
         move_intervalo = setInterval(moverEsq, 10)
     }
-    if(!movimentando && (key == "d" || key == "arrowright" || this.id == "dir")){
+    if(!movimentando && (key == "d" || key == "arrowright" )){
         //execultando movimento para a direita
         movimentando = true
         move_intervalo = setInterval(moverDir, 10)
     }
-    if(!movimentando && (key == "w" || key == "arrowup" || this.id == "top")){
+    if(!movimentando && (key == "w" || key == "arrowup")){
         //execultando movimento para a cima
         movimentando = true
         move_intervalo = setInterval(moverCima, 10)
     }
-    if(!movimentando && (key == "s" || key == "arrowdown" || this.id == "bai")){
+    if(!movimentando && (key == "s" || key == "arrowdown")){
+        //execultando movimento para a baixo
+        movimentando = true
+        move_intervalo = setInterval(moverBaixo, 10)
+    }
+}
+
+function inicairMoveBotao(){
+    if( this.id == "esq"){
+        //execultando movimento para esquerda
+        movimentando = true
+        move_intervalo = setInterval(moverEsq, 10)
+    }
+    if(this.id == "dir"){
+        //execultando movimento para a direita
+        movimentando = true
+        move_intervalo = setInterval(moverDir, 10)
+    }
+    if(this.id == "top"){
+        //execultando movimento para a cima
+        movimentando = true
+        move_intervalo = setInterval(moverCima, 10)
+    }
+    if(this.id == "bai"){
         //execultando movimento para a baixo
         movimentando = true
         move_intervalo = setInterval(moverBaixo, 10)
@@ -76,6 +102,9 @@ function pararMove(){
         Teclas especificadas:
         Movimento esquerdo: 'a' ou seta esquerda
         Movimenot direito: 'd' ou seta esquerda
+        Movimento top: 'w' ou seta para cima
+        Movimento baixo: 's' ou seta para baixo
+        ou para o os botões precionados
     */
     movimentando = false
     clearInterval(move_intervalo)
