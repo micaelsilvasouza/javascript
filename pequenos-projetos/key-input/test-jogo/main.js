8649782685d4be849cd0cb50e0b2842c35924ef6
micaelsilvasouza <108833081+micaelsilvasouza@users.noreply.github.com>
2023-06-16 12:28:49 -0300

    Adicionando botões para celular

--- pequenos-projetos/key-input/test-jogo/main.js
+++ pequenos-projetos/key-input/test-jogo/main.js
@@ -1,23 +1,20 @@
 //Codidgo Principal do jogo
 //Recebendo o input text para ser o verificador de tecla
 let input = document.getElementById("input")
-
+let botoes = document.querySelectorAll(".botao")
 
 //Dimenções da tela
 let larg = window.innerWidth
 let altu = window.innerHeight
 
-
 //Mostradores de TIMER e PONTOS
 let timer = document.getElementById("timer")
 let pontos = document.getElementById("pontos")
-
 
 //iniciando o timer e variavel de agregção ao timer
 let agrega_time = 1
 time = 10
 iniciarTimer()
-
 
 //Definindo a variavel de verificadação de movimento
 let movimentando = false
@@ -26,6 +23,11 @@ let move_intervalo
 //Adicionado os eventos e funções do elemento input
 input.addEventListener("keydown", inicairMove)
 input.addEventListener("keyup", pararMove)
+//Adicionado os eventos e funçõe aos elementos buttons da classe botao
+for(let botao of botoes){
+    botao.addEventListener("touchstart", inicairMove)
+    botao.addEventListener("touchend", pararMove)
+}
 
 function inicairMove(event){
     /* 
@@ -39,22 +41,22 @@ function inicairMove(event){
     key = event.key.toLowerCase()//tecla precionada coloca em minusculo
     //console.log(key)
     //Verificando a tecla precionada
-    if(!movimentando && (key == "a" || key == "arrowleft")){
+    if(!movimentando && (key == "a" || key == "arrowleft" || this.id == "esq")){
         //execultando movimento para esquerda
         movimentando = true
         move_intervalo = setInterval(moverEsq, 10)
     }
-    if(!movimentando && (key == "d" || key == "arrowright")){
+    if(!movimentando && (key == "d" || key == "arrowright" || this.id == "dir")){
         //execultando movimento para a direita
         movimentando = true
         move_intervalo = setInterval(moverDir, 10)
     }
-    if(!movimentando && (key == "w" || key == "arrowup")){
+    if(!movimentando && (key == "w" || key == "arrowup" || this.id == "top")){
         //execultando movimento para a cima
         movimentando = true
         move_intervalo = setInterval(moverCima, 10)
     }
-    if(!movimentando && (key == "s" || key == "arrowdown")){
+    if(!movimentando && (key == "s" || key == "arrowdown" || this.id == "bai")){
         //execultando movimento para a baixo
         movimentando = true
         move_intervalo = setInterval(moverBaixo, 10)
