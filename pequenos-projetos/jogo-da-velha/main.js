e695e2b460513827665110c7494f7927f3383b96
micaelsilvasouza <108833081+micaelsilvasouza@users.noreply.github.com>
2023-07-02 13:15:18 -0300

    Condição de velha e rejogar

--- pequenos-projetos/jogo-da-velha/main.js
+++ pequenos-projetos/jogo-da-velha/main.js
@@ -1,5 +1,6 @@
 let campos = document.querySelectorAll("td")
 let formas = document.querySelectorAll("svg")
+let men = document.getElementById("men-vitoria")
 let jogador1 = []
 let jogador2 = []
 let jogador = 1
@@ -16,26 +17,35 @@ for(let id = 0; id < campos.length; id++){
 }
 
 function adicinarForma(){
+    let ganhou = []
     if(!this.escolhido && jogador == 1){
         this.figuras[0].style.visibility = "visible"
         this.escolhido = true
         jogador1.push(this.posicao)
         jogador = 2
-        vitoria(jogador1)
+        ganhou = verficarVitoria(jogador1)
+        ganhou[0]?vitoria(ganhou[1], "Jogador 1"): 0
     }else if(!this.escolhido && jogador == 2){
         this.figuras[1].style.visibility = "visible"
         this.figuras[2].style.visibility = "visible"
         this.escolhido = true
         jogador2.push(this.posicao)
         jogador = 1
-        vitoria(jogador2)
+        ganhou = verficarVitoria(jogador2)
+        ganhou[0]?vitoria(ganhou[1], "Jogador 2"): 0
+    }
+    
+    if(!ganhou[0] && jogador1.length >= 5){
+        men.children[0].innerHTML = "Deu Velha<br>Ninguém Ganhou"
+        men.style.display = "block"
     }
 }
 
 
-function vitoria(jogador) {
+function verficarVitoria(jogador) {
     let campeao = false
     let total = 0
+    let id = 0
     let condicoes = [
         [1,2,3],
         [4,5,6],
@@ -52,10 +62,13 @@ function vitoria(jogador) {
             for(let termo of condicao){
                 if(jogador.includes(termo)){
                     total++
+                }else{
+                    break
                 }
 
                 if(total >= 3){
                     campeao = true
+                    id = condicoes.indexOf(condicao)
                     break
                 }
             }
@@ -63,22 +76,33 @@ function vitoria(jogador) {
             total = 0
 
             if(campeao){
-                alert("Temos um ganhador")
-                window.location = "index.html"
                 break
             }
         }
     }
 
-    return campeao
+    return [campeao, id]
 }
 
-function fazerlinha(){
+function vitoria(id, jogador){
     document.querySelector("#linha").style.display = "block"
     let linha = document.querySelector("#linha").getContext("2d")
-    linha.moveTo(0,0)
-    linha.lineTo(308,320)
+    
+    let direções = [
+        [0,54,308,54],
+        [0,160,308,160],
+        [0,266,308,266],
+        [52,0,52,320],
+        [154,0,154,320],
+        [256,0,256,320],
+        [0,0,308,320],
+        [308,0,0,320]
+    ]
+    linha.moveTo(direções[id][0],direções[id][1])
+    linha.lineTo(direções[id][2], direções[id][3])
     linha.lineWidth = 10
     linha.strokeStyle = '#f00'
     linha.stroke()
+    men.children[0].innerHTML = "Parabéns "+jogador+"<br> Você Ganhou"
+    men.style.display = "block"
 }
