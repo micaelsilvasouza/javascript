3bec9cfbdc67ba385106af634386d63e397e7db1
micaelsilvasouza <108833081+micaelsilvasouza@users.noreply.github.com>
2023-07-10 13:19:04 -0300

    Nivel 4 e logica para sempre dar velha

--- pequenos-projetos/jogo-da-velha/main.js
+++ pequenos-projetos/jogo-da-velha/main.js
@@ -2,11 +2,16 @@ let campos = document.querySelectorAll("td")
 let formas = document.querySelectorAll(".formas")
 let men = document.getElementById("men-vitoria")
 let ganhou = [false, 0,false]
-let nivel = "n1"
+let nivel = "n4"
 let jogador1 = []
 let jogador2 = []
 let jogador = 1
 
+//Escolhedo como o computador vai se comportar no nivel 4
+let comportar = "velha"
+console.log(comportar)
+
+//adicionado a função principal da todo os campos
 for(let id = 0; id < campos.length; id++){
     campos[id].addEventListener('click', adicinarForma)
     campos[id].posicao = id
@@ -18,7 +23,7 @@ for(let id = 0; id < campos.length; id++){
     campos[id].escolhido = false
 }
 
-
+//função pricipal que é adicinada a cada um dos campos
 function adicinarForma(){
     if(!this.escolhido && nivel == "n1"){
         nivel1(this.posicao) 
@@ -33,23 +38,29 @@ function adicinarForma(){
         nivel3() 
     }
     
+    if(!this.escolhido && nivel == "n4" && !ganhou[0]){
+        nivel1(this.posicao) 
+        nivel4() 
+    }
+    
     if(!ganhou[0] && jogador1.length >= 5){
         men.children[0].innerHTML = "Deu Velha<br>Ninguém Ganhou"
         men.style.display = "block"
     }
 }
 
+//função para cada um dos niveis de jogo
 function nivel1(id){
     if(jogador == 1){
-        campos[id].figuras[0].style.visibility = "visible"
+        campos[id].figuras[1].style.visibility = "visible"
+        campos[id].figuras[2].style.visibility = "visible"
         campos[id].escolhido = true
         jogador1.push(campos[id].posicao)
         jogador = 2
         ganhou = verficarVitoria(jogador1)
         ganhou[0]?vitoria(ganhou[1], "Jogador 1"): 0
     }else if(jogador == 2){
-        campos[id].figuras[1].style.visibility = "visible"
-        campos[id].figuras[2].style.visibility = "visible"
+        campos[id].figuras[0].style.visibility = "visible"
         campos[id].escolhido = true
         jogador2.push(campos[id].posicao)
         jogador = 1
@@ -89,6 +100,21 @@ function nivel3(){
     adicinarFormaComputador(campo)
 }
 
+function nivel4() {
+    let campo = undefined
+    let chance = verficarVitoria(jogador2)[2]
+    if(chance[0]){
+        campo = finalizar(chance)
+    }
+    
+    if(comportar == "velha" && campo == undefined){
+        campo = velha()
+    }
+
+    adicinarFormaComputador(campo)
+}
+
+//Funções auxilares para o computador
 function finalizar(chance) {
     let campo = undefined
     for(let i = 1; i < chance.length; i++){
@@ -114,12 +140,33 @@ function impedir() {
 
 function escolherAleatorio() {
     let campo
-    //escolhendo uma posição aleataria
-    while (true) {
-        campo = campos[Math.floor(Math.random() * 9)]
-        if(!campo.escolhido){
-            break
+    if(jogador2.length < 4){
+        //escolhendo uma posição aleataria
+        while (true) {
+            campo = campos[Math.floor(Math.random() * 9)]
+            if(!campo.escolhido){
+                break
+            }
         }
+    }
+
+    return campo
+}
+
+function velha() {
+    let campo = undefined
+    if(jogador2.length == 0 && !campos[4].escolhido){
+        campo = campos[4]
+    }else if(jogador2.length == 0 && campos[4].escolhido){
+        campo = campos[0]
+    }else if(jogador1.length == 2 && jogador2.includes(0) && campos[8].escolhido){
+        campo = campos[2]
+    }else if(jogador2.length > 0){
+        campo = impedir()
+    }
+
+    if(campo == undefined){
+        campo = escolherAleatorio()
     }
 
     return campo
@@ -129,8 +176,7 @@ function adicinarFormaComputador(campo) {
     if(!ganhou[0]){
         //Colocando depois de um tempo a figura
         setTimeout(() => {
-            campo.figuras[1].style.visibility = "visible"
-            campo.figuras[2].style.visibility = "visible"
+            campo.figuras[0].style.visibility = "visible"
             campo.escolhido = true
             jogador2.push(campo.posicao)
             jogador = 1
@@ -140,6 +186,8 @@ function adicinarFormaComputador(campo) {
     }
 }
 
+
+///Funções Para Vitoria de um jogador
 function verficarVitoria(jogador) {
     let campeao = false
     let perto = [false]
